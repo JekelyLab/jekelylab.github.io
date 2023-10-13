f2ba4942f914fc51807d458c7b1d8b97c742d1db
Gaspar Jekely <g.jekely@exeter.ac.uk>
2023-10-13 16:37:36 +0200

    del xarin

--- addons/macros.js
+++
@@ -1,4 +0,0 @@
-remark.macros['abs'] = function(width="30%", left="85%", top="15%", cl="") {
-  var url = this;
-  return '<img src="' + url + '" style="position:absolute;left:' + left + ';top:' + top + ';width:' + width + '" class="' + cl + '" />';
-  };
